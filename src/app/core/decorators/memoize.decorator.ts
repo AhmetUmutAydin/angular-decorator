2da3caceec5b11d51709b370587d90dd7fe8b828
AhmetUmutAydin <AhmetUmutAydin@gmail.com>
2023-01-12 22:58:38 +0300

    1 file modified

--- src/app/core/decorators/memoize.decorator.ts
+++ src/app/core/decorators/memoize.decorator.ts
@@ -3,9 +3,9 @@ import * as memoizee from 'memoizee';
 export function memoize() {
   return function (target: any, propertyKey: string, descriptor: any) {
     const orginalMethod = descriptor.value;
-    const method = memoizee(orginalMethod);
+    const memoizee = memoizee(orginalMethod);
     descriptor.value = function () {
-      return method.apply(this, arguments);
+      return memoizee.apply(this, arguments);
     };
   };
 }
