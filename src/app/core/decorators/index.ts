088de1ed4f0dee24281a476a2a75ffd2b55c0ecd
AhmetUmutAydin <AhmetUmutAydin@gmail.com>
2023-01-12 23:23:39 +0300

    1 file modified

--- src/app/core/decorators/index.ts
+++ src/app/core/decorators/index.ts
@@ -2,4 +2,3 @@ export * from './mockup.decorator';
 export * from './rest-api.decorator';
 export * from './cache.decorator';
 export * from './memoize.decorator';
-export * from './performance.decorator';
